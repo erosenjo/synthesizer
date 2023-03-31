668790f01adaf3e386af570cf6ec73f69f759bf1
erosenjo <erosenjo@oberlin.edu>
2023-03-31 14:14:38 -0400

    added information to tui.c regarding purpose and tutorial used.

--- tui.c
+++ tui.c
@@ -1,5 +1,10 @@
 #include <ncurses.h>
 
+/*
+ * This file handles the tui. For now it is very general, because I am still learning about ncurses.
+ * I am following the tutorial found here: https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/index.html
+ */
+
 int main() {
 	initscr(); //start curses mode
 	endwin(); //end curses mode
