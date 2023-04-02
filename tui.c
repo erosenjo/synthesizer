212d9199a02cdb709170b9540cef6025deaa37ca
erosenjo <erosenjo@oberlin.edu>
2023-04-02 11:45:14 -0400

    more work on tui

--- tui.c
+++ tui.c
@@ -6,6 +6,11 @@
  * I am following the tutorial found here: https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/index.html
  */
 
+#define DIST_FROM_TOP_TO_TITLE 3
+#define DIST_FROM_TITLE_TO_MENU 3
+#define DIST_FROM_MENU_TO_BOTTOM 2
+#define DIST_FROM_MENU_TO_SIDES 5
+
 int main() {
 
 	/* VARIABLES */
@@ -27,11 +32,17 @@ int main() {
 
 	getmaxyx(stdscr,row,col); //get size of window
 
-	mvprintw(5,(col-strlen(msg))/2,msg); //move printing cursor to (y,x) and print
+	mvprintw(DIST_FROM_TOP_TO_TITLE,(col-strlen(msg))/2,msg); //move printing cursor to (y,x) and print
 	refresh(); //prints the buffer to the screen
 	
 	//making a menu
-	
+    int menuy = row - (DIST_FROM_TITLE_TO_MENU + DIST_FROM_TOP_TO_TITLE + DIST_FROM_MENU_TO_BOTTOM);
+    int menux = col - (DIST_FROM_MENU_TO_SIDES * 2);
+    int starty = DIST_FROM_TITLE_TO_MENU + DIST_FROM_TOP_TO_TITLE; //origin y coordinate for menu
+    int startx = DIST_FROM_MENU_TO_SIDES; //origin x coordinate for menu
+	WINDOW *menu = newwin(menuy, menux, starty, startx);
+    wborder(menu, '|', '|', '-', '-', '+', '+', '+', '+');
+    wrefresh(menu);
 
 	getch(); //waits for user input
 	endwin(); //end curses mode
