db4e18e5c200276977401841ebfaa4d487ac915f
Ben Snyder <bsnyder2@users.noreply.github.com>
2023-05-09 21:48:08 -0400

    Bigger

--- tui.hpp
+++ tui.hpp
@@ -8,8 +8,8 @@ class TUI
 private:
     static const int WINDOW_Y_OFFSET = 2;
     static const int WINDOW_X_OFFSET = 2;
-    static const int WINDOW_HEIGHT = 8;
-    static const int WINDOW_WIDTH = 50;
+    static const int WINDOW_HEIGHT = 16;
+    static const int WINDOW_WIDTH = 128;
 
     static const int DISPLAY_STEP = SampleGenerator::N_SAMPLES / WINDOW_WIDTH;
     static constexpr double SCALE_FACTOR = WINDOW_HEIGHT / ((double)255 * 2);
